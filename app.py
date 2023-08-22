4c251299b3870e3c0166578426067cd31fe2248d
Raph CAymo <131464626+raphcaymo@users.noreply.github.com>
2023-08-22 10:39:06 +0800

    Updated Map
    added some legend and feature

---
+++ app.py
@@ -0,0 +1,266 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+import json
+
+# Opens the geojson file for the countries and saves it into countries_geojson
+with open('data/countries.geojson') as f:
+   countries_geojson = json.load(f)
+
+# Database setup
+engine = create_engine("sqlite:///data/energy.db")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Create an app, being sure to pass __name__
+app = Flask(__name__)
+Energy_table = Base.classes.energy
+
+######################### Create a list of all countries ######################
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# Query all the distinct countries
+results = session.query(Energy_table.country).distinct().all()
+
+session.close()
+
+# Create a list to hold all country names
+all_countries = []
+
+for country in results:
+    all_countries.append(country[0])
+
+###############################################################################
+
+# Define what to do when a user hits the index route
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return (f"Project 3 API<br/>"
+            f"Available routes:<br/>"
+            f"/api/v1.0/countries_geojson  (This is the link for the geojson of the countries)<br/>"
+            f"/api/v1.0/data  (Complete data for all countries)<br/>"
+            f"/api/v1.0/countries  (List of all countries)<br/>"
+            f"/api/v1.0/country_data/(country_name)<br/>"
+            f"/api/v1.0/country_data/(country_name)/(data)<br/>"
+            "Options for data:<br/>"
+            "access_to_elec<br/>"
+            "elec_from_renew<br/>"
+            "low_carbon_elec<br/>" 
+            "co2_emissions<br/>"
+            "primary_energy_cons")
+
+@app.route("/api/v1.0/countries")
+def countries():
+    # Print out all the countries
+    return jsonify(all_countries)
+
+@app.route("/api/v1.0/countries_geojson")
+def country_geojson():
+    return jsonify(countries_geojson)
+
+@app.route("/api/v1.0/data")
+def all_data():
+
+    complete_country_data = []
+
+    for country_name in all_countries:
+        # Create our session (link) from Python to the DB
+        session = Session(engine)
+
+        # These are the columns we want for the graph. 
+        want_columns = ['country', 'year', 'access_to_elec', 'elec_from_fossil', 'elec_from_renew',
+                    'low_carbon_elec', 'co2_emissions', 'primary_energy_cons']
+
+        # Query for the country and its data on the database
+        country = session.query(Energy_table.country).filter(Energy_table.country == country_name).distinct().all()[0][0]
+        results = session.query(Energy_table.country, Energy_table.year, Energy_table.access_to_elec, Energy_table.elec_from_fossil,
+        Energy_table.elec_from_renew, Energy_table.low_carbon_elec, Energy_table.co2_emissions,
+        Energy_table.primary_energy_cons).filter(Energy_table.country == country_name).all()
+
+        # Close the session
+        session.close()
+
+        # Create a dictionary to jsonify for the API
+        country_data = {'country': country, 'year': {}}
+        for row in results:
+            # row[1] is the year
+            country_data['year'][str(row[1])] = {}
+
+            # We start at 2 as the first 2 elements of the list are the country name and the year
+            for i in range(2,len(row)):
+                country_data['year'][str(row[1])][want_columns[i]] = float(row[i])
+
+        complete_country_data.append(country_data)
+
+    return jsonify(complete_country_data)
+
+
+@app.route("/api/v1.0/country_data/<country_name>")
+def country_all_data(country_name):
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # These are the columns we want for the graph. 
+    want_columns = ['country', 'year', 'access_to_elec', 'elec_from_fossil', 'elec_from_renew',
+                 'low_carbon_elec', 'co2_emissions', 'primary_energy_cons']
+    if country_name in all_countries:
+
+        # Query for the country and its data on the database
+        country = session.query(Energy_table.country).filter(Energy_table.country == country_name).distinct().all()[0][0]
+        results = session.query(Energy_table.country, Energy_table.year, Energy_table.access_to_elec, Energy_table.elec_from_fossil,
+        Energy_table.elec_from_renew, Energy_table.low_carbon_elec, Energy_table.co2_emissions,
+        Energy_table.primary_energy_cons).filter(Energy_table.country == country_name).all()
+
+        # Close the session
+        session.close()
+
+        # Create a dictionary to jsonify for the API
+        country_data = [{country: {}}]
+        country_dict = {'year': {}}
+        for row in results:
+
+            # row[1] is the year
+            country_dict['year'][str(row[1])] = {}
+
+            # We start at 2 as the first 2 elements of the list are the country name and the year
+            for i in range(2,len(row)):
+                country_dict['year'][str(row[1])][want_columns[i]] = row[i]
+
+        country_data[0][country] = country_dict
+
+        return jsonify(country_data)
+    else:
+        # Close the session
+        session.close()
+        return jsonify({"Error": f"Country cannot be found. Try to use /api/v1.0/countries link for country names"}), 404
+
+@app.route("/api/v1.0/country_data/<country_name>/<data>")
+def country_data(country_name, data):
+
+    if (data == 'access_to_elec') and (country_name in all_countries):
+        # Create our session (link) from Python to the DB
+        session = Session(engine)
+
+        # Query the country name and the results for which data is chosen
+        country = session.query(Energy_table.country).filter(Energy_table.country == country_name).distinct().all()[0][0]
+        results = session.query(Energy_table.country, Energy_table.year, Energy_table.access_to_elec).\
+        filter(Energy_table.country == country_name).all()
+
+        # Close the session
+        session.close
+
+        # Create a list of dictionaries to jsonify the data
+        country_data = [{country: {'access_to_elec': {}}}]
+        for row in results:
+            country_data[0][country]['access_to_elec'][row[1]] = row[2]
+        return jsonify(country_data)
+
+    elif (data == 'elec_from_fossil') and (country_name in all_countries):
+        # Create our session (link) from Python to the DB
+        session = Session(engine)
+
+        # Query the country name and the results for which data is chosen
+        country = session.query(Energy_table.country).filter(Energy_table.country == country_name).distinct().all()[0][0]
+        results = session.query(Energy_table.country, Energy_table.year, Energy_table.elec_from_fossil).\
+        filter(Energy_table.country == country_name).all()
+
+        # Close the session
+        session.close
+
+        # Create a list of dictionaries to jsonify the data
+        country_data = [{country: {'elec_from_fossil': {}}}]
+        for row in results:
+            country_data[0][country]['elec_from_fossil'][row[1]] = row[2]
+        return jsonify(country_data)
+
+    elif (data == 'elec_from_renew') and (country_name in all_countries):
+        # Create our session (link) from Python to the DB
+        session = Session(engine)
+
+        # Query the country name and the results for which data is chosen
+        country = session.query(Energy_table.country).filter(Energy_table.country == country_name).distinct().all()[0][0]
+        results = session.query(Energy_table.country, Energy_table.year, Energy_table.elec_from_renew).\
+        filter(Energy_table.country == country_name).all()
+
+        # Close the session
+        session.close
+
+        # Create a list of dictionaries to jsonify the data
+        country_data = [{country: {'elec_from_renew': {}}}]
+        for row in results:
+            country_data[0][country]['elec_from_renew'][row[1]] = row[2]
+        return jsonify(country_data)
+
+    elif (data == 'low_carbon_elec') and (country_name in all_countries):
+        # Create our session (link) from Python to the DB
+        session = Session(engine)
+
+        # Query the country name and the results for which data is chosen
+        country = session.query(Energy_table.country).filter(Energy_table.country == country_name).distinct().all()[0][0]
+        results = session.query(Energy_table.country, Energy_table.year, Energy_table.low_carbon_elec).\
+        filter(Energy_table.country == country_name).all()
+
+        # Close the session
+        session.close
+
+        # Create a list of dictionaries to jsonify the data
+        country_data = [{country: {'low_carbon_elec': {}}}]
+
+        for row in results:
+            country_data[0][country]['low_carbon_elec'][row[1]] = row[2]
+        return jsonify(country_data)
+
+    elif (data == 'co2_emissions') and (country_name in all_countries):
+        # Create our session (link) from Python to the DB
+        session = Session(engine)
+
+        # Query the country name and the results for which data is chosen
+        country = session.query(Energy_table.country).filter(Energy_table.country == country_name).distinct().all()[0][0]
+        results = session.query(Energy_table.country, Energy_table.year, Energy_table.co2_emissions).\
+        filter(Energy_table.country == country_name).all()
+
+        # Close the session
+        session.close
+
+        # Create a list of dictionaries to jsonify the data
+        country_data = [{country: {'co2_emissions': {}}}]
+        for row in results:
+            country_data[0][country]['co2_emissions'][row[1]] = row[2]
+        return jsonify(country_data)
+
+    elif (data == 'primary_energy_cons') and (country_name in all_countries):
+        # Create our session (link) from Python to the DB
+        session = Session(engine)
+
+        # Query the country name and the results for which data is chosen
+        country = session.query(Energy_table.country).filter(Energy_table.country == country_name).distinct().all()[0][0]
+        results = session.query(Energy_table.country, Energy_table.year, Energy_table.primary_energy_cons).\
+        filter(Energy_table.country == country_name).all()
+
+        # Close the session
+        session.close
+
+        # Create a list of dictionaries to jsonify the data
+        country_data = [{country: {'primary_energy_cons': {}}}]
+        for row in results:
+            country_data[0][country]['primary_energy_cons'][row[1]] = row[2]
+        return jsonify(country_data)
+    
+    else:
+        return jsonify({"Error": f"Country/data cannot be found. Try to use /api/v1.0/countries link for country names or \
+                        the list in the main route for the data list"}), 404
+    
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ js/script_merged.js
@@ -0,0 +1,302 @@
+// Creating the map object
+let myMap = L.map("map", {
+  center: [26.8206, 30.8025],
+  zoom: 1
+});
+
+
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+
+// Use this link to get the GeoJSON data. This makes use of the data from the Flask API
+let link = "http://127.0.0.1:5000/api/v1.0/countries_geojson";
+let dataLink = "http://127.0.0.1:5000/api/v1.0/data"
+
+
+
+// Uncomment if you want to use the local files
+// let link = "data/countries.geojson";
+// let dataLink = "data/energy.json"
+
+
+
+
+
+// The function that will determine the color of a neighborhood based on the borough that it belongs to
+
+function chooseColor(Continent) {
+  return Continent == "Asia" ? "yellow" :
+          Continent == "North America"  ? "red" :
+          Continent == "South America"  ? "Blue" :
+          Continent == "Africa"  ? "green" :
+          Continent == "Europe" ? "purple" :
+          Continent == "Oceania" ? "lightblue" :
+          Continent == "Antartica" ? "white" :
+                    "lightgray";
+}
+
+
+// Getting our GeoJSON data
+d3.json(link).then(function (data) {
+
+  // Initialize graphs
+  initialCountry = "Canada"
+  updateLineGraph(initialCountry)
+  barchart(initialCountry)
+
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data, {
+    // Styling each feature (in this case, a neighborhood)
+    style: function (feature) {
+      return {
+        color: "white",
+        // Call the chooseColor() function to decide which color to color our neighborhood. (The color is based on the borough.)
+        fillColor: chooseColor(feature.properties.Continent),
+        fillOpacity: 0.5,
+        weight: 1.5
+      };
+    },
+
+
+    // This is called on each feature.
+    onEachFeature: function (feature, layer) {
+      // Set the mouse events to change the map styling.
+      layer.on({
+        /* When a user's mouse cursor touches a map feature, the mouseover
+        event calls this function, which makes that feature's opacity change to 90% so that it stands out. */
+        mouseover: highlightFeature,
+
+        /* When the cursor no longer hovers over a map feature (that is, when the mouseout event occurs), 
+        the feature's opacity reverts back to 50%. */
+        mouseout: resetHighlight,
+
+        // When a feature (neighborhood) is clicked, it enlarges to fit the screen.
+        click: function (event) {
+          myMap.fitBounds(event.target.getBounds());
+          let selected_country = feature.properties.Country;
+          //console.log(selected_country);
+
+          // bubblechart(selected_country);
+          updateLineGraph(selected_country)
+          barchart(selected_country);
+        }
+      });
+
+
+      // Giving each feature a popup with information that's relevant to it
+      layer.bindPopup("<h2>" + feature.properties.Country + "</h2> <hr> <h4> Continent: " + feature.properties.Continent + "</h4>" +
+      "<h4> Country Code: " + feature.properties.ISO_A3 + "</h4>");
+      ;
+    }
+  }).addTo(myMap);
+}
+
+);
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////// Function for line graph ///////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+// The code below was commented off to work with the country selection by clicking on the map
+
+// Extract relevant data for the selected country
+
+// let countryData = data.find(country => country.country === "Canada");
+// let countryDropdown = document.getElementById('country-selector');
+
+// Iterate through the data and create options for each country
+// data.forEach(country => {
+//     let option = document.createElement('option');
+//     option.value = country.country;
+//     option.textContent = country.country;
+//     countryDropdown.appendChild(option);
+// });
+// Add an event listener to the dropdown
+// countryDropdown.addEventListener('change', function () {
+//     let selectedCountry = this.value;
+//     updateLineGraph(selectedCountry);
+// });
+
+
+function updateLineGraph(selectedCountry) {
+  // Fetch the selected country's data from energy.json
+  d3.json(dataLink).then(function (data) {
+    countryData = data.find(country => country.country === selectedCountry);
+
+    // Extract years and energy access values
+    years = Object.keys(countryData.year);
+    accessValues = years.map(year => countryData.year[year].access_to_elec);
+
+    // Update the line graph using Plotly.update
+    
+    Plotly.newPlot('line-graph', {
+      x: [years], // Update x-axis
+      y: [accessValues], // Update y-axis
+      type: 'lines+markers', // Update chart type
+      name: selectedCountry, // Update legend label
+    });
+
+    // Create the Plotly Line Chart
+    let trace = {
+      x: years,
+      y: accessValues,
+      mode: 'lines+markers',
+      name: 'Energy Access',
+      marker: {
+        size: 8
+      }
+    };
+
+    let layout = {
+      title: 'Energy Access Over Years',
+      xaxis: {
+        title: 'Year'
+      },
+      yaxis: {
+        title: 'Energy Access (%)'
+      }
+
+    };
+    Plotly.newPlot('line-graph', [trace], layout);
+  });
+}
+
+
+/* Creating a Legend for Continent */
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (myMap) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = ["Asia","North America","South America", "Africa","Europe","Oceania","Antartica"],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + chooseColor(grades[i]) + '"></i> ' +
+            grades[i] + '<br>';
+    }
+
+    return div;
+};
+
+legend.addTo(myMap); 
+
+/*asdfasd*/
+var info = L.control();
+
+info.onAdd = function (myMap) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Country on cursor</h4>' +  (props ?
+        '<b>' + props.Country + '</b><br />'
+        : 'Hover over a country');
+};
+
+info.addTo(myMap);
+
+
+
+function highlightFeature(e) {
+  var layer = e.target;
+
+  layer.setStyle({
+      color: '#666',
+      dashArray: '',
+      fillOpacity: 0.9
+  });
+
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+      layer.bringToFront();
+  }
+
+  info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+  var layer = e.target;
+
+  layer.setStyle({
+      color: 'white',
+      dashArray: '',
+      fillOpacity: 0.5
+  });
+
+  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+      layer.bringToFront();
+  }
+
+  info.update(layer.feature.properties);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////// Function for bar graph ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+function barchart(selected_country){
+  d3.json(dataLink).then(function(data) {
+    let years = Object.keys(data[0].year);
+    let fossil_list = [];
+    let renew_list = [];
+    
+    for (let i=0; i<data.length; i++) {
+      // Determine which country's data you will access
+      if(data[i].country == selected_country) {
+        for (let j=0; j<years.length; j++) {
+          fossil_list.push(data[i].year[years[j]].elec_from_fossil);
+          renew_list.push(data[i].year[years[j]].elec_from_renew);
+        }
+        break
+      }
+    }
+    
+    //console.log(selected_country);
+    //console.log(fossil_list);
+    //console.log(renew_list);
+  
+    // Create chart
+    var trace1 = {
+      x: years,
+      y: fossil_list,
+      type: 'bar',
+      name: 'Fossil Fuel',
+      marker: {
+        color: 'rgb(0,0,0)',
+        opacity: 0.5
+      }
+    };
+
+    var trace2 = {
+      x: years,
+      y: renew_list,
+      type: 'bar',
+      name: 'Renewable',
+      marker: {
+        color: 'rgb(84,239,7)',
+        opacity: 0.5
+      }
+    };
+  
+    var data = [trace1, trace2];
+  
+    var layout = {
+      title: selected_country,
+      yaxis: {
+        title: 'Terawatt hour (TWh)'
+      }
+    };
+  
+    Plotly.newPlot('bar-chart', data, layout);
+  });
+}
+
